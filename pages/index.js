20dbdd50c28b7dc29793188fa1125ec9ab7b10d8
R&B Travels <myrnbtravels@gmail.com>
2026-05-16 04:31:44 -0700

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -1165,7 +1165,7 @@ section {
           }
           .home-text25 {
             font-size: 40px;
-            font-family: '' '' '' 'Inter' '' '' '';
+            font-family: '' '' '' '' Inter '' '' '' '';
             font-weight: 700;
           }
           .home-thq-timeline-content-elm2 {
